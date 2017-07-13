85c32bcdad51794c2d27ffb3eaecf0a04970bf36
Dmitry Medvedev <demon0900@mail.ru>
2017-07-13 17:00:37 +0600

    SYSV

--- src/main.c
+++ src/main.c
@@ -16,13 +16,13 @@ int main(){
 	snprintf(cln_name, MAX_NAME_SIZE, "client-%d", getpid());
 	printf("Client start\nClient name: %s\n", cln_name);
 
-        pthread_create(&srv_tid, NULL, srv_event_hndl, (void*)cln_name);
-        pthread_create(&cln_inc_tid, NULL, cln_inc_msg_hndl, (void*)cln_name);
-        pthread_create(&cln_out_tid, NULL, cln_out_msg_hndl, (void*)cln_name);
+	pthread_create(&srv_tid, NULL, srv_event_hndl, (void*)cln_name);
+	/*pthread_create(&cln_inc_tid, NULL, cln_inc_msg_hndl, (void*)cln_name);
+        pthread_create(&cln_out_tid, NULL, cln_out_msg_hndl, (void*)cln_name);*/
 
-        pthread_join(srv_tid, (void**)&srv_stat);
-        pthread_join(cln_inc_tid, (void**)&cln_inc_stat);
-        pthread_join(cln_out_tid, (void**)&cln_out_stat);
+	pthread_join(srv_tid, (void**)&srv_stat);
+	/*pthread_join(cln_inc_tid, (void**)&cln_inc_stat);
+	pthread_join(cln_out_tid, (void**)&cln_out_stat);*/
 
 	printf("Client: main thread bye\n");
 	exit(EXIT_SUCCESS);
